a865ed4759e46c31d5827b17e4d046d599341aee
Franco Catena <francocatena@gmail.com>
2015-06-21 01:32:54 -0300

    Set editor as readonly if script file is present

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,22 +1,35 @@
-$(document).on('ready page:load', function () {
-  if ($('.editor').length) {
-    var $editor   = $('.editor')
-    var $textarea = $('#script_text')
-    var editor    = ace.edit($editor.get(0))
++function () {
+  var editor = null
 
-    editor.setTheme('ace/theme/solarized_light')
-    editor.getSession().setMode('ace/mode/ruby')
+  $(document).on('ready page:load', function () {
+    if ($('.editor').length) {
+      var $editor   = $('.editor')
+      var $textarea = $('#script_text')
 
-    editor.setValue($textarea.val())
-    editor.getSession().setTabSize(2)
-    editor.getSession().setUseSoftTabs(true)
-    editor.setReadOnly($editor.data('readonly'))
-    editor.clearSelection()
-    editor.gotoLine(0, 0)
-    editor.setFontSize(14)
+      editor = ace.edit($editor.get(0))
 
-    editor.getSession().on('change', function () {
-      $textarea.val(editor.getValue())
-    })
-  }
-})
+      editor.setTheme('ace/theme/solarized_light')
+      editor.getSession().setMode('ace/mode/ruby')
+
+      editor.setValue($textarea.val())
+      editor.getSession().setTabSize(2)
+      editor.getSession().setUseSoftTabs(true)
+      editor.setReadOnly($editor.data('readonly'))
+      editor.clearSelection()
+      editor.gotoLine(0, 0)
+      editor.setFontSize(14)
+
+      editor.getSession().on('change', function () {
+        var text = editor.getValue()
+
+        $textarea.val(text)
+
+        $('#script_file').prop('disabled', !!text.trim())
+      })
+    }
+  })
+
+  $(document).on('change', '#script_file', function () {
+    editor && editor.setReadOnly($(this).val())
+  })
+}()
