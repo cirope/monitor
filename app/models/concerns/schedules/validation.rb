5188f52ae494d6e3913c780c35b6debfa527f64b
Franco Catena <francocatena@gmail.com>
2015-06-23 13:31:15 -0300

    Allow blank on schedule ends...

--- app/models/concerns/schedules/validation.rb
+++ app/models/concerns/schedules/validation.rb
@@ -4,7 +4,7 @@ module Schedules::Validation
   included do
     validates :script, :server, presence: true
     validates :start, presence: true, timeliness: { type: :datetime, on_or_after: :now }
-    validates :end, timeliness: { type: :datetime, after: :start }
+    validates :end, timeliness: { type: :datetime, after: :start }, allow_blank: true
     validates :interval, numericality: { only_integer: true, greater_than: 0 }, allow_blank: true
     validates :frequency, inclusion: { in: %w(minutes hourly daily weekly monthly) }, allow_blank: true
   end
