a1c35b4972b75f011e95f0d9fba717e70b0b4770
Franco Catena <francocatena@gmail.com>
2015-06-22 23:26:08 -0300

    Implement run helper test...

--- test/helpers/runs_helper_test.rb
+++ test/helpers/runs_helper_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class RunsHelperTest < ActionView::TestCase
   test 'run status' do
-    skip
+    assert_match /label-success/, run_status('ok')
+    assert_match /label-danger/, run_status('error')
+    assert_match /label-default/, run_status('pending')
   end
 end
