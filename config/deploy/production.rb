c7c783e9be3bc8b2287af7d491bdd030d232d2ac
Franco Catena <francocatena@gmail.com>
2015-06-23 00:58:55 -0300

    Change production domain to monitor.cirope.com

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,8 @@
 set :stage, :production
 set :rails_env, 'production'
 
-role :web, %w{deployer@monitor.com}
-role :app, %w{deployer@monitor.com}
-role :db,  %w{deployer@monitor.com}
+role :web, %w{deployer@monitor.cirope.com}
+role :app, %w{deployer@monitor.cirope.com}
+role :db,  %w{deployer@monitor.cirope.com}
 
-server 'monitor.com', user: 'deployer', roles: %w{web app db}
+server 'monitor.cirope.com', user: 'deployer', roles: %w{web app db}
