4fa95d015eddce6a4eb9c475d6c15299dc22fe00
Franco Catena <francocatena@gmail.com>
2015-06-23 02:46:06 -0300

    Mini aesthetic changes through the UI

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -1,6 +1,6 @@
 module SchedulesHelper
   def frequencies
-    %w(hourly daily weekly monthly).map do |frequency|
+    %w(minutes hourly daily weekly monthly).map do |frequency|
       [t("schedules.frequencies.#{frequency}"), frequency]
     end
   end
