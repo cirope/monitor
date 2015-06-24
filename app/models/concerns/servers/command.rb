a7ec0c09fd339b9ca2df18e69ef2e2579bb20fce
Franco Catena <francocatena@gmail.com>
2015-06-23 22:15:43 -0300

    Run script with an interactive shell

--- app/models/concerns/servers/command.rb
+++ app/models/concerns/servers/command.rb
@@ -9,7 +9,7 @@ module Servers::Command
     Net::SSH.start hostname, user, ssh_options do |ssh|
       ssh.exec! "chmod +x #{script_path}"
 
-      ssh.exec! "#{script_path}" do |channel, stream, data|
+      ssh.exec! "$SHELL -ci #{script_path}" do |channel, stream, data|
         status = 'ok' unless stream == :stderr
         output << data
       end
