3647f26d0eb1833590ebdb8a940881512255de4f
Franco Catena <francocatena@gmail.com>
2015-07-13 20:25:58 -0300

    Deliver later instead of now. Better sidekiq conf

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -10,7 +10,7 @@ class PasswordResetsController < ApplicationController
 
     if user
       user.prepare_password_reset
-      UserMailer.password_reset(user).deliver_now
+      UserMailer.password_reset(user).deliver_later
 
       redirect_to root_url, notice: t('.notice', scope: :flash)
     else
