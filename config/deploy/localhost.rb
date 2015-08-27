7be57feec161711a8b2f0d1f36f4c00fa49e5880
Franco Catena <francocatena@gmail.com>
2015-08-27 16:08:54 -0300

    Add localhost as a deploy target...

---
+++ config/deploy/localhost.rb
@@ -0,0 +1,13 @@
+set :application, 'monitor.mawidabp.com'
+set :rails_env, 'production'
+
+set :default_env, {
+  'http_proxy': 'http://172.28.16.125:3128',
+  'https_proxy': 'http://172.28.16.125:3128'
+}
+
+role :web, %w{deployer@127.0.0.1}
+role :app, %w{deployer@127.0.0.1}
+role :db,  %w{deployer@127.0.0.1}
+
+server '127.0.0.1', user: 'deployer', roles: %w{web app db}
