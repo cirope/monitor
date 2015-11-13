47044d65eebec36b4b3969c6d3acfd652de74af6
Franco Catena <francocatena@gmail.com>
2015-11-13 10:05:15 -0300

    Schedule sidekiq restart every 30 minutes

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,7 @@
 every 1.minutes do
-  runner 'Schedule.schedule'
-  runner 'Run.schedule'
+  runner 'Schedule.schedule; Run.schedule'
+end
+
+every 30.minutes do
+  command "cd #{path} && cap localhost sidekiq:restart"
 end
