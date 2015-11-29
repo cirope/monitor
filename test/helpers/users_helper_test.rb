4b5d64b74b174b6b550d5e2b68bd71a9adbc28da
Franco Catena <francocatena@gmail.com>
2015-11-28 23:37:09 -0300

    Add users role help

--- test/helpers/users_helper_test.rb
+++ test/helpers/users_helper_test.rb
@@ -4,4 +4,8 @@ class UsersHelperTest < ActionView::TestCase
   test 'roles' do
     assert_respond_to roles, :each
   end
+
+  test 'roles label' do
+    assert_match /href/, roles_label
+  end
 end
