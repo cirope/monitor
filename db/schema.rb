1c7b6aa1b9a3a268aacc0e83cdbd3fc26e77ee02
Franco Catena <francocatena@gmail.com>
2016-01-27 23:24:13 -0300

    Change session store to active record

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160128000229) do
+ActiveRecord::Schema.define(version: 20160128021745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -225,6 +225,16 @@ ActiveRecord::Schema.define(version: 20160128000229) do
   end
 
   add_index "servers", ["name"], name: "index_servers_on_name", using: :btree
+
+  create_table "sessions", force: :cascade do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
 
   create_table "subscriptions", force: :cascade do |t|
     t.integer  "issue_id",   null: false
