2376a0f89b16a452ab4a2d6d38a93e937400e006
Franco Catena <francocatena@gmail.com>
2016-02-12 10:42:11 -0300

    Add permalink to issues from board

--- app/controllers/issues/board_controller.rb
+++ app/controllers/issues/board_controller.rb
@@ -9,6 +9,7 @@ class Issues::BoardController < ApplicationController
   respond_to :html, :js
 
   def index
+    @issue_ids = issues.where(id: board_session).pluck 'id'
     @issues = issues.order(:created_at).where(id: board_session).page params[:page]
 
     respond_with @issues
