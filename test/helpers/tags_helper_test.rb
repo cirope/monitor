48948ae0f6c62a0441393ca311cfd5f4038f705f
Franco Catena <francocatena@gmail.com>
2016-02-26 14:51:46 -0300

    Add tags as a column on indexes

--- test/helpers/tags_helper_test.rb
+++ test/helpers/tags_helper_test.rb
@@ -4,4 +4,15 @@ class TagsHelperTest < ActionView::TestCase
   test 'tag_kinds' do
     assert_respond_to tag_kinds, :each
   end
+
+  test 'styles' do
+    assert_respond_to styles, :each
+  end
+
+  test 'tags' do
+    _tags  = scripts(:cd_root).tags
+    result = tags _tags
+
+    assert_match _tags.first.name, result
+  end
 end
