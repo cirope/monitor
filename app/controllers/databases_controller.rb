4255af41e4158cf81970f839cf1cfc875d205a6e
Franco Catena <francocatena@gmail.com>
2016-03-22 13:57:22 -0300

    Restrict author role

--- app/controllers/databases_controller.rb
+++ app/controllers/databases_controller.rb
@@ -1,5 +1,5 @@
 class DatabasesController < ApplicationController
-  before_action :authorize, :not_guest
+  before_action :authorize, :not_guest, :not_author
   before_action :set_title, except: [:destroy]
   before_action :set_database, only: [:show, :edit, :update, :destroy]
 
