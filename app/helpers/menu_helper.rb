b89a063e4a4637e2ffc682218f8348e284939657
Franco Catena <francocatena@gmail.com>
2016-04-20 11:18:28 -0300

    Fix menu for security and guest users

--- app/helpers/menu_helper.rb
+++ app/helpers/menu_helper.rb
@@ -1,7 +1,7 @@
 module MenuHelper
-  def menu_item_for model, path
+  def menu_item_for model, path, show_active = false
     link = link_to model.model_name.human(count: 0), path
-    active = model.model_name.route_key == controller_name
+    active = show_active || model.model_name.route_key == controller_name
 
     content_tag(:li, link, (active ? { class: 'active' } : {}))
   end
