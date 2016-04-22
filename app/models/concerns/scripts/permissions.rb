def81d70e4371516449a72c86679893405779c9d
Franco Catena <francocatena@gmail.com>
2016-04-22 12:13:33 -0300

    Only supervisors and maintainers can edit scripts

--- app/models/concerns/scripts/permissions.rb
+++ app/models/concerns/scripts/permissions.rb
@@ -2,6 +2,6 @@ module Scripts::Permissions
   extend ActiveSupport::Concern
 
   def can_be_edited_by? user
-    user.supervisor? || maintainers.blank? || users.include?(user)
+    user.supervisor? || users.include?(user)
   end
 end
