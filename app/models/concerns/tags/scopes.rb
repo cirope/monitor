290dc757ac6e580366471a040e7f5fa4b07a3110
Franco Catena <francocatena@gmail.com>
2016-07-11 19:17:22 -0300

    Add tags to users that match LDAP group names

--- app/models/concerns/tags/scopes.rb
+++ app/models/concerns/tags/scopes.rb
@@ -1,6 +1,10 @@
 module Tags::Scopes
   extend ActiveSupport::Concern
 
+  included do
+    scope :for_users, -> { where kind: 'user' }
+  end
+
   module ClassMethods
     def export export
       where "#{table_name}.options @> ?", { export: export }.to_json
