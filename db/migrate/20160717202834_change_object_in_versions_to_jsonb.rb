47c202b8124fcb52c45c2c9a17387d8176ec3793
Franco Catena <francocatena@gmail.com>
2016-07-17 17:38:05 -0300

    Update migration conversion to work on Postgre 9.4

--- db/migrate/20160717202834_change_object_in_versions_to_jsonb.rb
+++ db/migrate/20160717202834_change_object_in_versions_to_jsonb.rb
@@ -1,6 +1,11 @@
 class ChangeObjectInVersionsToJsonb < ActiveRecord::Migration[5.0]
-  def change
-    change_column :versions, :object, :jsonb
-    change_column :versions, :object_changes, :jsonb
+  def up
+    change_column :versions, :object, :jsonb, using: 'object::jsonb'
+    change_column :versions, :object_changes, :jsonb, using: 'object_changes::jsonb'
+  end
+
+  def down
+    change_column :versions, :object, :json, using: 'object::json'
+    change_column :versions, :object_changes, :json, using: 'object_changes::json'
   end
 end
