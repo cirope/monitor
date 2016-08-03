24227bc3215543f0d92acccb2e20b495916e08b5
Franco Catena <francocatena@gmail.com>
2016-08-03 16:19:38 -0300

    No more proxy on localhost

--- config/deploy/localhost.rb
+++ config/deploy/localhost.rb
@@ -1,11 +1,6 @@
 set :rails_env, 'production'
 set :branch, 'bsj'
 
-set :default_env, {
-  'http_proxy':  'http://10.0.0.14:8080',
-  'https_proxy': 'http://10.0.0.14:8080'
-}
-
 role :web, %w{deployer@127.0.0.1}
 role :app, %w{deployer@127.0.0.1}
 role :db,  %w{deployer@127.0.0.1}
