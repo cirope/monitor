7570541e4699491097085c6c73064cc29fde6b9e
Franco Catena <francocatena@gmail.com>
2016-09-01 23:54:44 -0300

    Cancel execution if previous run is still running

--- app/jobs/script_job.rb
+++ app/jobs/script_job.rb
@@ -4,24 +4,25 @@ class ScriptJob < ApplicationJob
   def perform run
     return if run.canceled?
 
-    job      = run.job
-    schedule = run.schedule
-
-    run.update! status: 'running', started_at: Time.zone.now
-
-    Run.transaction do
-      out  = { status: 'canceled' }
-      out  = job.server.execute job.script if schedule.run?
-      data = ActiveSupport::JSON.decode out[:output] rescue nil
-
-      run.update!(
-        status:   out[:status],
-        output:   out[:output],
-        data:     data,
-        ended_at: Time.zone.now
-      )
-
-      run.execute_triggers
-    end
+    run.should_be_canceled? ? run.cancel : execute(run)
   end
+
+  private
+
+    def execute run
+      job      = run.job
+      schedule = run.schedule
+
+      run.mark_as_running
+
+      Run.transaction do
+        out  = { status: 'canceled' }
+        out  = job.server.execute job.script if schedule.run?
+        data = ActiveSupport::JSON.decode out[:output] rescue nil
+
+        run.finish status: out[:status], output: out[:output], data: data
+
+        run.execute_triggers
+      end
+    end
 end
