36530257f9fc2df3a30a834f53757dba8deaadd2
Franco Catena <francocatena@gmail.com>
2016-09-23 11:27:10 -0300

    Fix link to add and remove all issues to board

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -130,6 +130,6 @@ module IssuesHelper
   private
 
     def issues_board_path_with_params
-      issues_board_path filter: filter_params, script_id: @script.id
+      issues_board_path filter: filter_params.to_h, script_id: @script.id
     end
 end
