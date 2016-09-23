9bcfcec931b9fd3f3bd0306a8a2ca8debc8a5e01
Franco Catena <francocatena@gmail.com>
2016-09-23 11:13:59 -0300

    Fix overdue runs cancelation

--- app/models/concerns/schedules/cleanup.rb
+++ app/models/concerns/schedules/cleanup.rb
@@ -4,8 +4,8 @@ module Schedules::Cleanup
   def cleanup
     all_destroyed = true
 
-    self.class.transaction do
-      runs.executed.or(runs.canceled).or(runs.overdue).find_each do |run|
+    Schedule.transaction do
+      cleanable_runs.find_each do |run|
         destroyed = run.destroy
 
         all_destroyed &&= destroyed
@@ -14,4 +14,19 @@ module Schedules::Cleanup
 
     all_destroyed
   end
+
+  private
+
+    def cleanable_runs
+      times = case frequency
+      when 'minutes'
+        10
+      when 'hours'
+        4
+      else
+        1
+      end
+
+      runs.executed.or(runs.canceled).or(runs.overdue_by interval * times, frequency)
+    end
 end
