b105995fc38c7732886e0aed2486276f3bc3cf2e
Franco Catena <francocatena@gmail.com>
2016-12-13 00:16:34 -0300

    Capistrano 3.7.0

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,6 @@ set :log_level, :info
 
 set :deploy_to, "/var/www/#{fetch(:application)}"
 set :deploy_via, :remote_cache
-set :scm, :git
 
 set :linked_files, %w{config/application.yml}
 set :linked_dirs, %w{log public/uploads private}
