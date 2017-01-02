475787ad11fca7a0ade8c543f3ccf46ee80a451e
Franco Catena <francocatena@gmail.com>
2017-01-02 08:40:20 -0300

    Use Ruby 2.3.0 on deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :linked_files, %w{config/application.yml}
 set :linked_dirs, %w{log public/uploads private tmp/pids}
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.3.3'
+set :rbenv_ruby, '2.3.0'
 
 set :keep_releases, 5
 
