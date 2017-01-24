1286a029ee900dc43a38f0fffbd17c9ee8481409
Franco Catena <francocatena@gmail.com>
2017-01-24 13:52:39 -0300

    Only restart sidekiq if process not found

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,8 +2,11 @@ every 1.minutes do
   runner 'Schedule.schedule; Run.schedule'
 end
 
-every 30.minutes do
-  command "cd #{path} && bundle exec cap localhost sidekiq:restart"
+every 1.minutes do
+  check   = '(ps aux | grep sidekiq | grep -v grep) > /dev/null'
+  restart = "(cd #{path} && bundle exec cap localhost sidekiq:restart)"
+
+  command "#{check} || #{restart}"
 end
 
 every 1.day do
