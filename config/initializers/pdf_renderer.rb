745fc5c4b0a074eb32b1a516e9ddcddc4200e176
Franco Catena <francocatena@gmail.com>
2017-12-04 11:59:49 -0300

    Support spaces on generated PDFs

--- config/initializers/pdf_renderer.rb
+++ config/initializers/pdf_renderer.rb
@@ -3,7 +3,7 @@ ActionController::Renderers.add :pdf do |object, options|
   filename = object.respond_to?(:pdf_name) ? object.pdf_name : object.to_s
   response = send_file path,
     type:        :pdf,
-    disposition: "attachment; filename=#{filename}.pdf"
+    disposition: "attachment; filename=\"#{filename}.pdf\""
 
   FileRemoveJob.set(wait: 15.minutes).perform_later path
 
