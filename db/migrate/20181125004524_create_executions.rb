fa23e8560939f7c707ff50d28a8b49717e8229eb
Franco Catena <francocatena@gmail.com>
2019-01-09 00:15:09 -0300

    Apply suggestions from code review
    Co-Authored-By: Shelvak <nestorcoppi@gmail.com>

--- db/migrate/20181125004524_create_executions.rb
+++ db/migrate/20181125004524_create_executions.rb
@@ -1,13 +1,13 @@
 class CreateExecutions < ActiveRecord::Migration[5.2]
   def change
     create_table :executions do |t|
-      t.integer :script_id, null: false, index: true
-      t.integer :server_id, null: false
+      t.references :script, index: true, null: false
+      t.references :server, index: true, null: false
       t.string :status, default: Execution.statuses[:pending]
       t.datetime :started_at
       t.datetime :ended_at
       t.text :output
-      t.integer :user_id, null: false
+      t.references :user, index: true, null: false
 
       t.timestamps
     end
