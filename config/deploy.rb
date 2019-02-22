d32f2d953a5c220fae68d725a7baec2d1ce62f23
Franco Catena <francocatena@gmail.com>
2019-02-22 00:27:32 -0300

    Run rake tenant:default on cap deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,6 +18,7 @@ set :keep_releases, 5
 
 namespace :deploy do
   before :check,      'config:upload'
+  before :publishing, :tenant
   after  :publishing, :restart
   after  :finishing,  :move_files
   after  :finishing,  :cleanup
