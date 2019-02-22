9b544103913a3155e7492d9e8628ec6c6a3e0517
Franco Catena <francocatena@gmail.com>
2019-02-22 00:23:13 -0300

    DB Seed now contains default account

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,20 @@
-User.create!(
-  name: 'Admin',
-  lastname: 'Istrator',
-  username: 'admin',
-  email: 'admin@monitor.com',
-  role: 'supervisor',
-  password: '123',
-  password_confirmation: '123'
-)
+ActiveRecord::Base.transaction do
+  if Account.where(tenant_name: 'default').empty?
+    Current.account = Account.create!(
+      name:        Membership.human_attribute_name('default'),
+      tenant_name: 'default'
+    )
+
+    Apartment::Tenant.switch Current.account.tenant_name do
+      User.create!(
+        name:                  'Admin',
+        lastname:              'Admin',
+        username:              'admin',
+        email:                 'admin@monitor.com',
+        role:                  'supervisor',
+        password:              '123',
+        password_confirmation: '123'
+      )
+    end
+  end
+end
