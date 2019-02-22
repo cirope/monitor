b05d4d40d7b411d16509fad66cb46f46acd51783
Franco Catena <francocatena@gmail.com>
2019-02-22 10:01:22 -0300

    Point production branch to new-init-multi-tenant

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :stage, :production
 set :rails_env, 'production'
-set :branch, 'new-init'
+set :branch, 'new-init-multi-tenant'
 
 role :web, %w{deployer@demo.greditsoft.com}
 role :app, %w{deployer@demo.greditsoft.com}
