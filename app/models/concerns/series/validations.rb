483b8ab70d709e8754b416c41421e5f9058cd8b6
Franco Catena <francocatena@gmail.com>
2019-04-08 21:37:52 -0300

    Frozen strings on new files

--- app/models/concerns/series/validations.rb
+++ app/models/concerns/series/validations.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Series::Validations
   extend ActiveSupport::Concern
 
--- app/models/serie.rb
+++ app/models/serie.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Serie < ApplicationRecord
   establish_connection :metrics
 
