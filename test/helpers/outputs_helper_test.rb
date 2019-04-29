475f952299419c05d41ffbc058fea16e4903afbe
Franco Catena <francocatena@gmail.com>
2019-04-29 11:39:20 -0300

    Add output helper test

---
+++ test/helpers/outputs_helper_test.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class OutputsHelperTest < ActionView::TestCase
+  test 'link to error line' do
+    script = scripts :cd_root
+    error  = {
+      error: 'Test error',
+      line:  1
+    }
+
+    assert_match script.id.to_s, link_to_error_line(script, error, true)
+  end
+end
