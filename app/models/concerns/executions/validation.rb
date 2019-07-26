0ba07fc54a1bd18dd7c2849ec6993f9da75cbdab
Néstor Coppi <nestorcoppi@gmail.com>
2019-07-26 09:29:25 -0300

    Making remote executions possible (#16)

--- app/models/concerns/executions/validation.rb
+++ app/models/concerns/executions/validation.rb
@@ -6,12 +6,5 @@ module Executions::Validation
   included do
     validates :script, :server, :user, presence: true
     validates :pid, numericality: { only_integer: true }, allow_blank: true
-    validate :server_should_be_local
   end
-
-  private
-
-    def server_should_be_local
-      errors.add :server, :should_be_local unless server&.local?
-    end
 end
