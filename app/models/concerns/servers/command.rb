f5c3030d693b157d6eeefcb54ac302877bfc5458
Néstor Coppi <nestorcoppi@gmail.com>
2019-08-03 00:50:26 -0300

    Refactor local/remote exec Run/Execution

--- app/models/concerns/servers/command.rb
+++ app/models/concerns/servers/command.rb
@@ -13,11 +13,13 @@ module Servers::Command
       }
     end
 
-    if local?
-      execute_local  run, script_path
-    else
-      execute_remote run, script_path
+    output = []
+
+    status = decide_and_exec run, script_path do |data|
+      output << data
     end
+
+    { status: status, output: output.compact.join }
   rescue => ex
     { status: 'error', output: ex.to_s }
   end
@@ -25,18 +27,24 @@ module Servers::Command
   def execution execution
     script_path = execution.script.copy_to self
 
-    status = if local?
-               local_execution execution, script_path
-             else
-               remote_execution execution, script_path
-             end
+    decide_and_exec execution, script_path do |line|
+      execution.new_line line
+    end
+  end
 
-    if execution.killed?
+  def decide_and_exec executable, script_path
+    method = local? ? :local_exec : :remote_exec
+    exit_status = send(method, script_path, executable) do |line|
+      yield line
+    end
+
+    if executable.reload.killed?
       'killed'
-    elsif status.zero?
-      'success'
+    elsif exit_status.zero?
+      # Migra
+      executable.class.statuses[:ok] || 'success'
     else
-      execution.new_line "Exit status: #{status}"
+      yield "Exit status: #{exit_status}"
 
       'error'
     end
@@ -48,61 +56,13 @@ module Servers::Command
       "#{Rails.root}/bin/rails"
     end
 
-    def execute_local run, script_path
-      status      = ''
-      exit_status = 1
-      output      = StringIO.new
-
-      Open3.popen2e rails, 'runner', script_path do |stdin, stdout, thread|
-        run.update! pid: thread.pid
-
-        stdout.each { |line| output << line }
-
-        exit_status = thread.value.exitstatus.to_i
-      end
-
-      output << "\nExit status: #{exit_status}" unless exit_status.zero?
-
-      status = if run.killed?
-                 'killed'
-               elsif exit_status.zero?
-                 'ok'
-               else
-                 'error'
-               end
-
-      {
-        status: status,
-        output: output.string.presence
-      }
-    end
-
-    def execute_remote run, script_path
-      out = Net::SSH::Connection::Session::StringWithExitstatus.new '', 0
-
-      Net::SSH.start hostname, user, ssh_options do |ssh|
-        ssh.exec! "chmod +x #{script_path}"
-
-        out = ssh.exec! "$SHELL -ci #{script_path}"
-
-        ssh.exec! "rm #{script_path}"
-      end
-
-      status_text = "\nExit status: #{out.exitstatus}" unless out.exitstatus.zero?
-
-      {
-        status: out.exitstatus.zero? ? 'ok' : 'error',
-        output: out.to_s + status_text.to_s
-      }
-    end
-
-    def local_execution execution, script_path
+    def local_exec script_path, executable
       status = 1
 
       Open3.popen2e rails, 'runner', script_path do |stdin, stdout, thread|
-        execution.update! pid: thread.pid
+        executable.update! pid: thread.pid
 
-        stdout.each { |line| execution.new_line line }
+        stdout.each { |line| yield "#{line.strip}\n" if line }
 
         status = thread.value.exitstatus.to_i
       end
@@ -110,15 +70,17 @@ module Servers::Command
       status
     end
 
-    def remote_execution execution, script_path
+    def remote_exec script_path, _executable = nil
       status = 1
 
       Net::SSH.start hostname, user, ssh_options  do |ssh|
-        # execution permission
+        # script permission
         ssh.exec! "chmod +x #{script_path}"
 
         status = ssh_exec_with_pty ssh, "$SHELL -c #{script_path}" do |data|
-          execution.new_line data
+          data.to_s.split("\n").each do |line|
+            yield "#{line}\n"
+          end
         end
 
         # Clean the script
