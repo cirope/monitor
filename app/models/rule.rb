54be6b874e9501227b5b4668e5a20845bb3091f4
Néstor Coppi <nestorcoppi@gmail.com>
2019-08-03 20:42:56 -0300

    Unifying exportable concern

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -3,10 +3,10 @@
 class Rule < ApplicationRecord
   include Auditable
   include Attributes::Strip
+  include Exportable
   include Exporter
   include Filterable
   include SearchableByName
-  include Rules::Export
   include Rules::Import
   include Rules::JSON
   include Rules::Scopes
