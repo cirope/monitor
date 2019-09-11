2487690f94677df5ed297626fab62c664d62d461
Franco Catena <francocatena@gmail.com>
2019-09-11 16:54:17 -0300

    No more X-Accel-Redirect for now

--- config/initializers/sendfile.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.config.middleware.swap(
-  ::Rack::Sendfile,
-  ::Rack::Sendfile, 'X-Accel-Redirect', [
-    ["#{Rails.root}/private", 'private_files'],
-    ["#{Rails.root}/storage", 'storage_files']
-  ]
-)
