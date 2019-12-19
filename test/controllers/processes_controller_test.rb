b57d9c00dba3b6b0fa864e74a53aea678bb34297
Franco Catena <francocatena@gmail.com>
2019-12-19 19:20:18 -0300

    Wait even more on Travis

--- test/controllers/processes_controller_test.rb
+++ test/controllers/processes_controller_test.rb
@@ -62,7 +62,7 @@ class ProcessesControllerTest < ActionController::TestCase
   private
 
     def sleep_process interval: 0.01
-      interval *= 30 if ENV['TRAVIS']
+      interval *= 100 if ENV['TRAVIS']
 
       detached_process command: "sleep #{interval}"
     end
