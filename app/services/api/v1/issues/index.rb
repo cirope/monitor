1e13d2856ac8eb89096b5823e54be1145d36aead
seba-hullop <sebastian@hullop.com>
2021-08-26 12:10:35 -0300

    agrego url al json de devolucion y tambien cambio de cuenta en current cuando se llama a la api

--- app/services/api/v1/issues/index.rb
+++ app/services/api/v1/issues/index.rb
@@ -4,9 +4,9 @@ class Api::V1::Issues::Index
   def call(params)
     issues_validator = Api::V1::Issues::Validator.new params
     if issues_validator.valid?
-      account = Account.find_by! tenant_name: params[:account_id]
+      Current.account = Account.find_by! tenant_name: params[:account_id]
 
-      account.switch!
+      Current.account.switch!
 
       query = Api::V1::Issues::Filter.new.call(query: Issue.all, params: params)
 
@@ -20,9 +20,9 @@ class Api::V1::Issues::Index
 
     def construct issues
       if issues.can_collapse_data?
-        data = issues.map { |issue| issue.converted_data.first.merge status: issue.status }
+        data = issues.map { |issue| issue.converted_data.first.merge status: issue.status, url: issue.url }
       else
-        data = issues.map { |issue| issue.to_json }
+        data = issues.to_json(methods: :url, except: %i[data options run_id])
       end
 
       Api::V1::StandardResponse.new.call data: data, code: 200
