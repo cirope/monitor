3c8d2ea7004983110a3ae3ce8c6c38f3ea864371
seba-hullop <sebastian@hullop.com>
2021-11-17 15:14:23 -0300

    cambio validacion

--- app/models/concerns/users/licenses.rb
+++ app/models/concerns/users/licenses.rb
@@ -4,15 +4,15 @@ module Users::Licenses
   extend ActiveSupport::Concern
 
   included do
-    before_save :check_licensed_user_limit, if: :licensed_user?
+    before_save :check_licensed_user_limit, if: :should_check_licensed_user?
 
-    scope :licensed_user, -> { where(role: ['manager', 'author', 'supervisor']).count }
+    scope :licensed_user, -> { where(role: ['manager', 'author', 'supervisor']) }
   end
 
   private
 
     def check_licensed_user_limit
-      if licensed_user >= licensed_user_limit
+      if licensed_user.count >= licensed_user_limit
         Rails.logger.info I18n.t('errors.messages.logger_licensed_user_limit'),
                           limit: licensed_user_limit
         errors.add :base, :licensed_user_limit
@@ -26,7 +26,19 @@ module Users::Licenses
         Rails.application.credentials.max_licensed_users_count[:global]
     end
 
-    def licensed_user?
+    def should_check_licensed_user?
+      new_record? || change_to_licensed_user?
+    end
+
+    def change_to_licensed_user?
+      was_not_licensed_user? && going_to_be_a_licensed_user?
+    end
+
+    def was_not_licensed_user?
+      ['manager', 'author', 'supervisor'].exclude? role_was
+    end
+
+    def going_to_be_a_licensed_user?
       manager? || author? || supervisor?
     end
 end
