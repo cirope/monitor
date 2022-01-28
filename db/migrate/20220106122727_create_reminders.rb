98f31bf60f05a93405fc8af767a720137b587afa
seba-hullop <sebastian@hullop.com>
2022-01-28 11:24:03 -0300

    agrego index a state de reminder

--- db/migrate/20220106122727_create_reminders.rb
+++ db/migrate/20220106122727_create_reminders.rb
@@ -2,7 +2,7 @@ class CreateReminders < ActiveRecord::Migration[6.0]
   def change
     create_table :reminders do |t|
       t.datetime :due_at, null: false, index: true
-      t.string :state_class_type, null: false
+      t.string :state_class_type, null: false, index: true
       t.jsonb :transition_rules
       t.references :issue, null: false, index: true, foreign_key: {
         on_delete: :restrict, on_update: :restrict
