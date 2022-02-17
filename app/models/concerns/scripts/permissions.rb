a4471cf47e5867dbf0a8a1c026b2761e05790959
Sergio <asergiop21@gmail.com>
2022-02-17 16:58:06 -0300

    Fix review

--- app/models/concerns/scripts/permissions.rb
+++ app/models/concerns/scripts/permissions.rb
@@ -4,12 +4,6 @@ module Scripts::Permissions
   extend ActiveSupport::Concern
 
   def can_be_edited_by? user
-    user.supervisor? || users.include?(user) || allow_tag_edit?
+    user.supervisor? || users.include?(user)
   end
-
-  private
-
-    def allow_tag_edit?
-      tags.select(&:export_edit?).present?
-    end
 end
