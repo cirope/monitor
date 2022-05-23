32d56494d0ebda2588d04546466e25d8bb60f853
Franco Catena <francocatena@gmail.com>
2022-05-23 13:01:07 -0300

    New default deploy options

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@
 
 set :stage, :production
 set :rails_env, 'production'
-set :branch, 'dashboards'
+set :branch, 'master'
 
 role :web, %w{deployer@demo.greditsoft.com}
 role :app, %w{deployer@demo.greditsoft.com}
