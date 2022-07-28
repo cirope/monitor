0fc5a6bf43b6d8fd1145f09c7bbdfca0536265c3
Ismael Isuani <ismaelisuani@gmail.com>
2022-07-28 12:42:12 -0300

    Fix SAML Groups Authorization

--- app/models/concerns/samls/response.rb
+++ app/models/concerns/samls/response.rb
@@ -6,21 +6,28 @@ module Samls::Response
     saml_response = OneLogin::RubySaml::Response.new response, settings: saml_config
 
     if saml_response.is_valid?
-      user = saml_user_for saml_response.nameid, saml_response.attributes
+      saml_user_for saml_response.nameid, saml_response.attributes
     end
   end
 
   private
 
     def saml_user_for email, attributes
+      result = nil
       pruned_attributes = prune_custom_attributes attributes
       pruned_attributes[:email] ||= email
 
       if user = find_user(pruned_attributes)
-        update_user user, pruned_attributes
-      end
+        role = extract_role pruned_attributes
 
-      user
+        if role
+          update_user user, role, pruned_attributes
+
+          return user, 'valid'
+        else
+          return user, 'not_authorized'
+        end
+      end
     end
 
     def find_user attributes
@@ -28,14 +35,14 @@ module Samls::Response
         User.find_by(username: attributes[:username]&.downcase)
     end
 
-    def update_user user, attributes
+    def update_user user, role, attributes
       User.transaction do
         user.update(
           username: attributes[:username],
           name:     attributes[:name],
           lastname: attributes[:lastname],
           email:    attributes[:email],
-          role:     extract_role(attributes),
+          role:     role,
           hidden:   false
         )
       end
