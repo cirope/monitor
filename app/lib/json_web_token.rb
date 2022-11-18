28132266b5fd626e03fd59bf69816b9932b227d7
seba-hullop <sebastian@hullop.com>
2022-11-18 12:49:34 -0300

    fix expiration token

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -2,8 +2,9 @@
 
 class JsonWebToken
   class << self
-    def encode(payload, exp = 24.hours.from_now)
-      payload[:exp] = exp.to_i
+    def encode(payload)
+      payload[:exp] = (payload[:exp] || 24.hours.from_now).to_i
+
       JWT.encode(payload, Rails.application.secrets.secret_key_base)
     end
 
