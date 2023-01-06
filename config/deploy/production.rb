6df3731fa7de10613bf2bd02df55aa1318b7c2f2
Sergio <asergiop21@gmail.com>
2023-01-06 09:22:37 -0300

    Gemfile.local setting

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,10 +2,10 @@
 
 set :stage, :production
 set :rails_env, 'production'
-set :branch, 'master'
+set :branch, 'c-arcos'
 
-role :web, %w{deployer@demo.greditsoft.com}
-role :app, %w{deployer@demo.greditsoft.com}
-role :db,  %w{deployer@demo.greditsoft.com}
+role :web, %w{deployer@arcos.greditsoft.com}
+role :app, %w{deployer@arcos.greditsoft.com}
+role :db,  %w{deployer@arcos.greditsoft.com}
 
-server 'demo.greditsoft.com', user: 'deployer', roles: %w{web app db}
+server 'arcos.greditsoft.com', user: 'deployer', roles: %w{web app db}
