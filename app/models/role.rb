71b4445eeea44626b27dbb5de8ac8872bebb01c2
Ismael Isuani <ismaelisuani@gmail.com>
2023-03-20 09:45:30 -0300

    Added identifier to roles

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,11 +2,12 @@ class Role < ApplicationRecord
   include Auditable
   include DisableSti
   include Attributes::Strip
+  include Roles::Identifer
   include Roles::Overrides
   include Roles::Relations
   include Roles::Scopes
   include Roles::Types
   include Roles::Validation
 
-  strip_fields :name, :description
+  strip_fields :name, :description, :identity
 end
