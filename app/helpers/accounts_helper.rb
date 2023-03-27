ca20f41fab51bb89c377534d495381df60ffb630
Ismael Isuani <ismaelisuani@gmail.com>
2023-03-27 12:12:01 -0300

    API token duration config by account (#141)

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module AccountsHelper
+  def token_frequencies
+    %w(minutes hours days weeks months years).map do |frequency|
+      [t("accounts.token_frequencies.#{frequency}"), frequency]
+    end
+  end
 end
--- app/models/concerns/accounts/options.rb
+++ app/models/concerns/accounts/options.rb
@@ -1,6 +1,16 @@
 module Accounts::Options
   extend ActiveSupport::Concern
 
+  attr_writer :token_interval, :token_frequency
+
+  included do
+    before_validation :assign_token_duration
+  end
+
+  def expire_token
+    eval "#{token_interval}.#{token_frequency}.from_now"
+  end
+
   def group_issues_by_schedule
     options&.fetch 'group_issues_by_schedule', nil
   end
@@ -8,6 +18,26 @@ module Accounts::Options
 
   def group_issues_by_schedule= value
     assign_option 'group_issues_by_schedule', value == true || value == '1'
+  end
+
+  def token_duration
+    options&.fetch 'token_duration', nil
+  end
+
+  def token_duration= value
+    assign_option 'token_duration', value
+  end
+
+  def token_interval
+    interval, frequency = token_duration&.first
+
+    @token_interval || interval || 1
+  end
+
+  def token_frequency
+    interval, frequency = token_duration&.first
+
+    @token_frequency || frequency || 'months'
   end
 
   private
@@ -19,5 +49,9 @@ module Accounts::Options
       options_will_change! unless prev_value == value
 
       self.options[name] = value
+    end
+
+    def assign_token_duration
+      self.token_duration = { token_interval => token_frequency }
     end
 end
