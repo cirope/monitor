b1c1faf0f27fd31ea142064ac93a877fa829f934
Ismael Isuani <ismaelisuani@gmail.com>
2023-05-15 16:52:47 -0300

    Fix CSP Header with ActiveStorage

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -8,7 +8,7 @@ Rails.application.config.content_security_policy do |policy|
   policy.default_src     :self
   policy.font_src        :self, :data
   policy.img_src         :self, :data
-  policy.object_src      :none
+  policy.object_src      :self
   policy.script_src      :self
   policy.style_src       :self
   policy.frame_ancestors :self
