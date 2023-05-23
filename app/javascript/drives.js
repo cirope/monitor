9c499a5bfc80cb064c6dc6593d476d031245836f
Franco Catena <85371+francocatena@users.noreply.github.com>
2023-05-23 16:36:52 -0300

    Update app/javascript/drives.js

--- app/javascript/drives.js
+++ app/javascript/drives.js
@@ -1,5 +1,5 @@
 $(document).on('change', '#drive_provider', function () {
-  var providerOptionsUrl = $(this).find(':selected').attr('data-drive-provider-options-url');
+  var providerOptionsUrl = $(this).find(':selected').attr('data-drive-provider-options-url')
 
-  $.getScript(providerOptionsUrl);
+  $.getScript(providerOptionsUrl)
 })
