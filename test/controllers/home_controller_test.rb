ff828bc85e61dc07a8482d714c1987857e3dd306
Ismael Isuani <ismaelisuani@gmail.com>
2023-06-22 14:26:37 -0300

    Added data filter on issues

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -130,12 +130,20 @@ class HomeControllerTest < ActionController::TestCase
   test 'should get filtered index using issue tags' do
     get :index, params: { filter: { tags: tags(:important).name } }
     assert_response :success
+
     assert_select 'table tbody'
   end
 
   test 'should get filtered index using issue user' do
     get :index, params: { filter: { user_id: users(:john).id } }
     assert_response :success
+    assert_select 'table tbody'
+  end
+
+  test 'should get filtered index using issue data' do
+    get :index, params: { filter: { data: { 'test': 'data' }.to_json } }
+    assert_response :success
+
     assert_select 'table tbody'
   end
 
