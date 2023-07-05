9971004b1d7577369161c10248c897bcfcc3c2fb
Sergio <asergiop21@gmail.com>
2023-07-05 14:42:08 -0300

    Modify localhost deploy

--- config/deploy/localhost.rb
+++ config/deploy/localhost.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 set :rails_env, 'production'
-
-set :default_env, {
-  'http_proxy': 'http://172.28.16.125:3128',
-  'https_proxy': 'http://172.28.16.125:3128'
-}
+set :branch, 'c-bancor-test'
 
 role :web, %w{deployer@127.0.0.1}
 role :app, %w{deployer@127.0.0.1}
