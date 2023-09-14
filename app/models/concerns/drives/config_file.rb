bf22dd6db268bae1b80f074b0247cb0d7a5dc858
Ismael Isuani <ismaelisuani@gmail.com>
2023-09-14 11:43:46 -0300

    Improved after commits

--- app/models/concerns/drives/config_file.rb
+++ app/models/concerns/drives/config_file.rb
@@ -2,9 +2,9 @@ module Drives::ConfigFile
   extend ActiveSupport::Concern
 
   included do
-    after_create_commit  :create_mount_point, :create_section, :create_systemd_file
-    after_update_commit  :update_section, :update_systemd_file
-    after_destroy_commit :delete_section, :delete_systemd_file
+    after_create_commit  :create_drive
+    after_update_commit  :update_drive
+    after_destroy_commit :destroy_drive
   end
 
   def section
@@ -25,31 +25,46 @@ module Drives::ConfigFile
 
   private
 
-    def create_mount_point
-      FileUtils.mkdir_p mount_point
+    def create_drive
+      create_mount_point
+      create_section
+      create_systemd_file
+      mount_drive
     end
 
-    def create_section
-      system 'rclone', *config_file_cmd('create', try("#{provider}_extra_params"))
-    end
-
-    def delete_mount_point
-      umount_drive
-
-      FileUtils.rmdir mount_point if Dir.exist?(mount_point)
-    end
-
-    def update_section
+    def update_drive
       umount_drive
 
       if saved_change_to_name?
+        delete_previous_mount_point
         delete_previous_section
-        create_section
+        delete_previous_systemd_file
+
+        create_drive
       else
         system 'rclone', *config_file_cmd('update')
       end
 
       mount_drive
+    end
+
+    def destroy_drive
+      umount_drive
+      delete_mount_point
+      delete_section
+      delete_systemd_file
+    end
+
+    def create_mount_point
+      FileUtils.mkdir_p mount_point
+    end
+
+    def delete_mount_point
+      FileUtils.rmdir mount_point if Dir.exist?(mount_point)
+    end
+
+    def create_section
+      system 'rclone', *config_file_cmd('create', try("#{provider}_extra_params"))
     end
 
     def delete_section
@@ -58,6 +73,12 @@ module Drives::ConfigFile
 
     def delete_previous_section
       system 'rclone', 'config', 'delete', previous_section
+    end
+
+    def delete_mount_point
+      previous_mount_point = File.join "#{Rails.root}/drives", previous_section
+
+      FileUtils.rmdir previous_mount_point if Dir.exist?(previous_mount_point)
     end
 
     def config_file_cmd action, extras = nil
