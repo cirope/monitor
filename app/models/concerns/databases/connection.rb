6cd1aa2b936f10e57a8e184a6988a80bba200f6d
Sergio <asergiop21@gmail.com>
2024-07-16 10:02:32 -0300

    Improvements

--- app/models/concerns/databases/connection.rb
+++ app/models/concerns/databases/connection.rb
@@ -6,18 +6,32 @@ module Databases::Connection
   end
 
   def connection_test
-    require 'odbc'
+    require 'odbc_utf8'
 
     begin
-      query = 'SELECT 1'
-      pool  = ODBC.connect(name)
+      query = query_by_driver driver
+
+      pool  = ODBC.connect(name, user, password)
 
       pool.run query
 
-    rescue ODBC::Error => e
-      errors.add :base, e.message
+    rescue ODBC::Error
+      errors.add :base, I18n.t('databases.error.check_connection')
 
       raise ActiveRecord::Rollback
     end
   end
+
+  private
+
+    def query_by_driver driver
+      case driver
+      when /PostgreSQL Unicode/i then 'Select version();'
+      when /FreeTDS/i            then 'Select @@VERSION;'
+      when /sqlite/i             then 'Select sqlite_version();'
+      when /Oracle/i             then 'Select * FROM v$version;'
+      else
+        'Select 1'
+      end
+    end
 end
