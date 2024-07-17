a8e921a7d4d83615dd8fa8c6505b25de6fd953e9
Sergio <asergiop21@gmail.com>
2024-07-17 16:50:47 -0300

    Fix review

--- app/models/concerns/databases/connection.rb
+++ app/models/concerns/databases/connection.rb
@@ -7,15 +7,15 @@ module Databases::Connection
 
   def try_connection
     begin
+      client = connect
 
-      unless connect
-        errors.add :base, I18n.t('databases.errors.try_query')
+      unless query client
+        errors.add :base, I18n.t('databases.errors.query')
 
         raise ActiveRecord::Rollback
       end
-
     rescue ODBC::Error
-      errors.add :base, I18n.t('databases.errors.check_connection')
+      errors.add :base, I18n.t('databases.errors.connection')
 
       raise ActiveRecord::Rollback
     end
@@ -24,18 +24,20 @@ module Databases::Connection
   private
 
     def connect
-      pool = if driver.downcase =~ /freetds/
-               ODBC.connect(name, user, password)
-             else
-               ODBC.connect(name)
-             end
+      if driver =~ /freetds/i
+        ODBC.connect name, user, password
+      else
+        ODBC.connect name
+      end
+    end
 
-      pool.do(test_query) == 1
+    def query client
+      client.do(test_query) == 1
     end
 
     def test_query
-      case driver
-      when /oracle/i then 'SELECT 1 FROM DUAL;'
+      if driver =~ /oracle/i
+        'SELECT 1 FROM DUAL;'
       else
         'SELECT 1;'
       end
