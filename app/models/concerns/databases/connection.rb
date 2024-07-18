e0f7a41fc476174b271942cef3938cf55f4e69f6
Sergio <asergiop21@gmail.com>
2024-07-18 16:04:34 -0300

    Improvements

--- app/models/concerns/databases/connection.rb
+++ app/models/concerns/databases/connection.rb
@@ -2,32 +2,32 @@ module Databases::Connection
   extend ActiveSupport::Concern
 
   included do
-    after_save :try_connection
-  end
-
-  def try_connection
-    begin
-      client = connect
-
-      unless client.run test_query
-        errors.add :base, :query
-
-        raise ActiveRecord::Rollback
-      end
-    rescue ODBC::Error
-      errors.add :base, :connection
-
-      raise ActiveRecord::Rollback
-    end
+    after_save :test_connection
   end
 
   private
 
+    def test_connection
+      begin
+        client = connect
+
+        unless client.run test_query
+          errors.add :base, :query
+
+          raise ActiveRecord::Rollback
+        end
+      rescue ODBC::Error
+        errors.add :base, :connection
+
+        raise ActiveRecord::Rollback
+      end
+    end
+
     def connect
       if driver =~ /freetds/i
-        ODBC.connect(name, user, password)
+        ODBC.connect name, user, password
       else
-        ODBC.connect(name)
+        ODBC.connect name
       end
     end
 
