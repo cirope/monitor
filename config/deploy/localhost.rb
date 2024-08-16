ce101a0d26dd7fbca524754a56d409cacf44921a
Sergio <asergiop21@gmail.com>
2024-08-16 11:42:39 -0300

    disable http proxy

--- config/deploy/localhost.rb
+++ config/deploy/localhost.rb
@@ -3,10 +3,10 @@
 set :rails_env, 'production'
 set :branch, 'c-bica'
 
- set :default_env, {
-   'http_proxy': 'http://172.20.208.12:8080',
-   'https_proxy': 'http://172.20.208.12:8080'
- }
+# set :default_env, {
+#   'http_proxy': 'http://172.20.208.12:8080',
+#   'https_proxy': 'http://172.20.208.12:8080'
+# }
 
 role :web, %w{deployer@127.0.0.1}
 role :app, %w{deployer@127.0.0.1}
