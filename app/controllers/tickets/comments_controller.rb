5aca52a71f01c22387d6d2b469cf489d375afee9
Sergio <asergiop21@gmail.com>
2024-08-28 20:30:02 -0300

    Fixes

--- app/controllers/tickets/comments_controller.rb
+++ app/controllers/tickets/comments_controller.rb
@@ -1,19 +1,9 @@
 # frozen_string_literal: true
 
-class Tickets::CommentsController < ApplicationController
-  include Authentication
-  include Authorization
-  include Issues::Filters
-  include Issues::Owner
+class Tickets::CommentsController < Issues::CommentsController
 
   before_action :set_issue
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
-
-  def show
-  end
-
-  def edit
-  end
 
   def create
     @comment = current_user.comments.new comment_params.merge issue_id: @issue.id
@@ -25,25 +15,9 @@ class Tickets::CommentsController < ApplicationController
     end
   end
 
-  def update
-    @comment.update comment_params
-  end
-
-  def destroy
-    @comment.destroy
-  end
-
   private
 
-    def set_issue
-      @issue = Ticket.find_by id: params[:ticket_id]
-    end
-
-    def set_comment
-      @comment = current_user.comments.find params[:id]
-    end
-
-    def comment_params
-      params.require(:comment).permit :text, :attachment
-    end
+  def set_issue
+    @issue = Ticket.find_by id: params[:ticket_id]
+  end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,8 +2,4 @@
 
 class Ticket < Issue
   self.table_name = "issues"
-
- # has_many :subscriptions, foreign_key: "issue_id"
- # has_many :users, through: :subscriptions
- # has_many :comments, dependent: :destroy, foreign_key: "issue_id"
 end
