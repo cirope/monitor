adcdde59db426ad535c2011ef346a0a50311cbf6
Sergio <asergiop21@gmail.com>
2024-09-05 19:28:14 -0300

    Fix tests

--- test/controllers/tickets/comments_controller_test.rb
+++ test/controllers/tickets/comments_controller_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 
 class Tickets::CommentsControllerTest < ActionController::TestCase
   setup do
-    @comment = comments :new_script
+    @comment = comments :possitive
 
     login
   end
@@ -12,7 +12,7 @@ class Tickets::CommentsControllerTest < ActionController::TestCase
   test 'should create comment' do
     assert_difference 'Comment.count' do
       post :create, params: {
-        issue_id: @comment.issue_id,
+        ticket_id: @comment.issue_id,
         comment: {
           text: @comment.text,
           attachment: fixture_file_upload('text.txt')
@@ -21,5 +21,33 @@ class Tickets::CommentsControllerTest < ActionController::TestCase
     end
 
     assert_redirected_to ticket_url(@comment.issue)
+  end
+
+  test 'should show comment' do
+    get :show, params: { ticket_id: @comment.issue_id, id: @comment }, xhr: true, as: :js
+    assert_response :success
+  end
+
+  test 'should get edit' do
+    get :edit, params: { ticket_id: @comment.issue_id, id: @comment }, xhr: true, as: :js
+    assert_response :success
+  end
+
+  test 'should update comment' do
+    patch :update, params: {
+      ticket_id: @comment.issue_id,
+      id: @comment,
+      comment: { text: 'Updated text' }
+    }, xhr: true, as: :js
+
+    assert_response :success
+  end
+
+  test 'should destroy comment' do
+    assert_difference('Comment.count', -1) do
+      delete :destroy, params: { ticket_id: @comment.issue_id, id: @comment }, xhr: true, as: :js
+    end
+
+    assert_response :success
   end
 end
