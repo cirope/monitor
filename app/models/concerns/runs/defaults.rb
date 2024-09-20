8a1b6929bf032023c4df457203faf0193b4b2a5a
Franco Catena <85371+francocatena@users.noreply.github.com>
2024-09-20 13:33:03 -0300

    Double default callback on runs (#235)

--- app/models/concerns/runs/defaults.rb
+++ app/models/concerns/runs/defaults.rb
@@ -2,6 +2,8 @@ module Runs::Defaults
   extend ActiveSupport::Concern
 
   included do
+    # Some flows trigger the validation callback and others the create callback
+    before_validation :set_defaults
     before_create :set_defaults
   end
 
