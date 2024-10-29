fc3de7b490f6e4a237ee4cc4ccb8b1414b97fa06
Sergio <asergiop21@gmail.com>
2024-10-29 09:18:29 -0300

    Fix virtual_env python

--- app/models/concerns/servers/command.rb
+++ app/models/concerns/servers/command.rb
@@ -152,7 +152,7 @@ module Servers::Command
         env_vars[variable.name] = variable.value
       end
 
-      if script.python? && local? && script.libraries.present?
+      if script.python? && local?
         env_vars['VIRTUAL_ENV'] = virtual_env script
         env_vars['PATH']        = "#{env_vars['VIRTUAL_ENV']}/bin:$PATH"
         env_vars['PYTHONPATH']  = "#{env_vars['VIRTUAL_ENV']}/lib/python#{LOCAL_PYTHON_VERSION}/site-packages"
