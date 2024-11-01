445e1223183a7addfc92fde0a1feb3eb21d7d6e1
Ismael Isuani <ismaelisuani@gmail.com>
2024-11-01 14:35:52 -0300

    Schema rollback+

--- db/migrate/20241101015805_change_canonical_data_to_json.rb
+++
@@ -1,7 +0,0 @@
-class ChangeCanonicalDataToJson < ActiveRecord::Migration[7.0]
-  def change
-    remove_index :issues, :canonical_data
-
-    change_column :issues, :canonical_data, :json, using: 'canonical_data::json'
-  end
-end
